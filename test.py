57e537bdfc228b44aa743828f74650b42281336f
Daniel Agorander <daniel.agorander@gmail.com>
2021-08-18 22:51:29 +0200

    Testing final bullshit

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 #! /bin/python3
 
-import socket
+#import socket
 from gpiozero import CPUTemperature # pip install gpiozero
 
-hostname = socket.gethostname()
+#hostname = socket.gethostname()
 #cpuTemp = CPUTemperature()
 cpu = CPUTemperature()
 print(cpu.temperature)
 
-print("Hostname is: " + hostname)
+#print("Hostname is: " + hostname)
 #print("CPU Temp is: " + cpuTemp.temperature + "C")
