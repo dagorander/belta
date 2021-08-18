1b703928c347f36890880770750976fb8211cf39
Daniel Agorander <daniel.agorander@gmail.com>
2021-08-18 22:48:27 +0200

    Testing cpu temp

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 #! /bin/python3
 
 import socket
-from gpiozero import CPUTemperature
+from gpiozero import CPUTemperature # pip install gpiozero
+
+
 
 hostname = socket.gethostname()
 cpuTemp = CPUTemperature()
 
 print("Hostname is: " + hostname)
-print("CPU Temp is: " + cpuTemp + "C")
+print("CPU Temp is: " + cpuTemp.temperature + "C")
