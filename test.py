c082faff0b3fd302b2da265e878c7369554fed40
Daniel Agorander <daniel.agorander@gmail.com>
2021-08-18 22:56:13 +0200

    Testing another thing

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
 #! /bin/python3
 
 #import socket
-from gpiozero import CPUTemperature # pip install gpiozero
+#from gpiozero import CPUTemperature # pip install gpiozero
+
+import vcgencmd
+CPUc=vcgencmd.measure_temp()
+print str(CPUc)
+
+chmod +x cputemp
+sudo cp cputemp /usr/bin
 
 #hostname = socket.gethostname()
 #cpuTemp = CPUTemperature()
-cpu = CPUTemperature()
-print(cpu.temperature)
+#cpu = CPUTemperature()
+#print(cpu.temperature)
 
 #print("Hostname is: " + hostname)
 #print("CPU Temp is: " + cpuTemp.temperature + "C")
