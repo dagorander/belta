5534147ee53f0df92b6d47cb1ad103a6b457f560
Daniel Agorander <daniel.agorander@gmail.com>
2021-08-19 22:56:20 +0200

    Making code a bit prettier and cleaning up some garbage

--- logger.sh
+++ logger.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# mpstat uses package sysstat
+# PACKAGES USED:
+# sysstat: needed for mpstat
 
-#First we take our base readings
-CPU_READING=$(cat /sys/class/thermal/thermal_zone0/temp)    #Did not need anything extra
-#CPU_UTILIZATION_RAW=$(mpstat -P ALL --dec=0)    #using package: sysstat
+
+CPU_READING=$(cat /sys/class/thermal/thermal_zone0/temp)
+
 
 # CPU Util numbers need to be split up per core/all
 # We use mpstat from sysstat with no decimals (because int is nicer than float) to get the numbers for all cores
@@ -15,6 +16,7 @@ CPU_1_IDLE=$(mpstat -P ALL --dec=0 | sed '1,5d' | sed '2,3d' | tr -s ' ' | cut -
 CPU_2_IDLE=$(mpstat -P ALL --dec=0 | sed '1,6d' | sed '2,2d' | tr -s ' ' | cut -d ' ' -f 12)
 CPU_3_IDLE=$(mpstat -P ALL --dec=0 | sed '1,7d' | sed '2,2d' | tr -s ' ' | cut -d ' ' -f 12)
 
+
 #Convert the Idle numbers to utilization numbers
 CPU_ALL_UTILIZATION=$((100-$CPU_ALL_IDLE))
 CPU_0_UTILIZATION=$((100-$CPU_0_IDLE))
@@ -22,16 +24,13 @@ CPU_1_UTILIZATION=$((100-$CPU_1_IDLE))
 CPU_2_UTILIZATION=$((100-$CPU_2_IDLE))
 CPU_3_UTILIZATION=$((100-$CPU_3_IDLE))
 
+
 #Then we make them sane to human eyes
 CPU_TEMP=$((CPU_READING/1000))
 
+
 #Output for devtesting
 echo "CPU TEMP: ${CPU_TEMP}c"
-#echo "$CPU_ALL_IDLE"
-#echo "$CPU_0_IDLE"
-#echo "$CPU_1_IDLE"
-#echo "$CPU_2_IDLE"
-#echo "$CPU_3_IDLE"
 echo ""
 echo "CPU UTILIZATION:"
 echo "ALL CORES: ${CPU_ALL_UTILIZATION}%"
