1c74f95221c4cac5327c3817e1ed3a64a886c264
Daniel Agorander <daniel.agorander@gmail.com>
2021-08-22 21:42:02 +0200

    Small progress, new problems. NExt up to solve is integer math to get RAM usage percentages.

--- logger.sh
+++ logger.sh
@@ -10,11 +10,11 @@ CPU_READING=$(cat /sys/class/thermal/thermal_zone0/temp)
 # We use mpstat from sysstat with no decimals (because int is nicer than float) to get the numbers for all cores
 # sed away the lines we don't need and remove duplicate spaces to make the idle field cuttable
 # We are using idle because the utilization is broken up into many types of util
-CPU_ALL_IDLE=$(mpstat -P ALL --dec=0 | sed '1,3d' | sed '2,5d' | tr -s ' ' | cut -d ' ' -f 12)
-CPU_0_IDLE=$(mpstat -P ALL --dec=0 | sed '1,4d' | sed '2,4d' | tr -s ' ' | cut -d ' ' -f 12)
-CPU_1_IDLE=$(mpstat -P ALL --dec=0 | sed '1,5d' | sed '2,3d' | tr -s ' ' | cut -d ' ' -f 12)
-CPU_2_IDLE=$(mpstat -P ALL --dec=0 | sed '1,6d' | sed '2,2d' | tr -s ' ' | cut -d ' ' -f 12)
-CPU_3_IDLE=$(mpstat -P ALL --dec=0 | sed '1,7d' | sed '2,2d' | tr -s ' ' | cut -d ' ' -f 12)
+CPU_ALL_IDLE=$(mpstat -P all --dec=0 | sed '1,3d' | tr -s ' ' | cut -d ' ' -f 12)
+CPU_0_IDLE=$(mpstat -P 0 --dec=0 | sed '1,3d' | tr -s ' ' | cut -d ' ' -f 12)
+CPU_1_IDLE=$(mpstat -P 1 --dec=0 | sed '1,3d' | tr -s ' ' | cut -d ' ' -f 12)
+CPU_2_IDLE=$(mpstat -P 2 --dec=0 | sed '1,3d' | tr -s ' ' | cut -d ' ' -f 12)
+CPU_3_IDLE=$(mpstat -P 3 --dec=0 | sed '1,3d' | tr -s ' ' | cut -d ' ' -f 12)
 
 
 #Convert the Idle numbers to utilization numbers
@@ -29,6 +29,16 @@ CPU_3_UTILIZATION=$((100-$CPU_3_IDLE))
 CPU_TEMP=$((CPU_READING/1000))
 
 
+# Here we use 'free' to get current RAM total and used, then calculate what the percentage used is
+RAM_TOTAL=$(free -m | sed '1,1d' | sed '2,2d' | tr -s ' ' | cut -d ' ' -f 2)
+RAM_USED=$(free -m | sed '1,1d' | sed '2,2d' | tr -s ' ' | cut -d ' ' -f 3)
+
+
+# Here we get ourselves some percentages
+# NEED TO FIND A WAY TO DO DECIMAL MATH IN BASH
+
+
+
 #Output for devtesting
 echo "CPU TEMP: ${CPU_TEMP}c"
 echo ""
@@ -38,3 +48,7 @@ echo "   CORE 0: ${CPU_0_UTILIZATION}%"
 echo "   CORE 1: ${CPU_1_UTILIZATION}%"
 echo "   CORE 2: ${CPU_2_UTILIZATION}%"
 echo "   CORE 3: ${CPU_3_UTILIZATION}%"
+echo ""
+echo "RAM TOTAL: ${RAM_TOTAL}MiB"
+echo "RAM USED: ${RAM_USED}MiB"
+echo "RAM USAGE: ${RAM_USE_PERCENTAGE}%"
